709cba0d537d7e685298e8d90f9d5ef07ac2b9e2
Thomas CERDERA <thomas.cerdera@infotel.com>
2024-09-26 16:42:08 +0200

    adding socket io to the logic

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 import express from "express"
 import { Server } from "socket.io";
+import { createServer } from 'node:http';
+import { dirname, join } from 'node:path';
+import cors from 'cors'
 
 const app = express();
 const port = 8000;
-const io = new Server(server);
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(cors({
+      origin: 'http://localhost:3000'
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on("join_room", (data)=> {
+        console.log(data)
+        socket.join(data);
+    })
+  });
+
+  server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-  });

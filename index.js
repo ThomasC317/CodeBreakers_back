5326a5a3bdaac569d4a03889acd827387d0dd562
Thomas CERDERA <thomas.cerdera@infotel.com>
2024-09-30 15:51:51 +0200

    adding lobby logic to the back

--- index.js
+++ index.js
@@ -1,84 +1,120 @@
-import express from "express"
+import express from "express";
 import { Server } from "socket.io";
-import { createServer } from 'node:http';
-import cors from 'cors'
+import { createServer } from "node:http";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 const server = createServer(app);
 const players = [];
+const MAX_PLAYERS_PER_LOBBY = 4;
 const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-    },
-  });
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 let numberToGuess = 0;
+const lobbies = [];
 
-app.use(cors({
-      origin: 'http://localhost:3000'
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.on("join_room", (data) => {
-      console.log('a player joined the room', data);
-      socket.join(data.room);
-  
-      players.push({ roomId: socket.id, player: data.username });
-  
-      io.in(data.room).emit('players_list', players);
-  
+  socket.on("join_room", (data) => {
+    const lobby = lobbies.find((lobby) => lobby.id === data.lobbyId);
+    if (
+      lobby &&
+      lobby.gameStatus === "waiting" &&
+      lobby.players.length < MAX_PLAYERS_PER_LOBBY
+    ) {
+      lobby.players.push({ roomId: socket.id, player: data.username });
+      socket.join(data.lobbyId);
+      io.in(data.room).emit("players_list", lobby.players);
       socket.emit("joined !");
-    });
-
-    socket.on("launch_game", (data) => {
-      console.log("game launched!");
-  
-      numberToGuess = generateRandomNumber(1, 1000); 
-      console.log("Number to guess: ", numberToGuess); 
-  
-      io.in(data.room).emit('number_to_guess', numberToGuess); 
-  
-      const firstPlayer = getFirstPlayerToPlay();
-      
-      io.to(firstPlayer.roomId).emit('your_turn', { message: "It's your turn!" }); 
-    });
-
-    socket.on("guess_number", (data) => {
-      console.log('Player guessed: ', data.guess);
-  
-      if (data.guess === numberToGuess) {
-        const index = players.findIndex(player => player.roomId === socket.id);
-        io.in(data.room).emit('victory', index); 
-      } else {
-        if (data.guess < numberToGuess) {
-          socket.emit('hint', { message: 'Too low!' });
-        } else if (data.guess > numberToGuess) {
-          socket.emit('hint', { message: 'Too high!' });
-        }
-  
-        const nextPlayerId = getNextPlayer(socket.id);
-        io.to(nextPlayerId).emit('your_turn', { message: "It's your turn!" }); 
-      }
-    });
-
-    socket.on("disconnect", () => {
-      console.log("disconnected!");
-  
-      const index = players.findIndex(player => player.roomId === socket.id);
-      if (index !== -1) players.splice(index, 1);
-  
-      io.emit('players_list', players); 
-    });
+    } else {
+      socket.emit("lobby_full_or_closed");
+    }
   });
 
-  server.listen(port, () => {
+  socket.on("create_lobby", (data) => {
+    const lobbyId = lobbies.length + 1;
+    const newLobby = {
+      lobbyId,
+      players: [],
+      gameStatus: "waiting",
+      name: data.name,
+    };
+    lobbies.push(newLobby);
+    socket.join(lobbyId);
+    io.to(lobbyId).emit("lobby_created", { lobbyId });
+  });
+
+  socket.on("get_lobbies", () => {
+    const availableLobbies = lobbies.filter(
+      (lobby) => lobby.gameStatus === "waiting"
+    );
+    socket.emit("lobbies_list", availableLobbies);
+  });
+
+  socket.on("launch_game", (data) => {
+    console.log("game launched!");
+
+    numberToGuess = generateRandomNumber(1, 1000);
+    console.log("Number to guess: ", numberToGuess);
+
+    io.in(data.room).emit("number_to_guess", numberToGuess);
+
+    const firstPlayer = getFirstPlayerToPlay();
+
+    io.to(firstPlayer.roomId).emit("your_turn", { message: "It's your turn!" });
+  });
+
+  socket.on("guess_number", (data) => {
+    console.log("Player guessed: ", data.guess);
+
+    if (data.guess === numberToGuess) {
+      const index = players.findIndex((player) => player.roomId === socket.id);
+      io.in(data.room).emit("victory", index);
+    } else {
+      if (data.guess < numberToGuess) {
+        socket.emit("hint", { message: "Too low!" });
+      } else if (data.guess > numberToGuess) {
+        socket.emit("hint", { message: "Too high!" });
+      }
+
+      const nextPlayerId = getNextPlayer(socket.id);
+      io.to(nextPlayerId).emit("your_turn", { message: "It's your turn!" });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected!");
+    lobbies.forEach((lobby) => {
+      const index = lobby.players.findIndex(
+        (player) => player.roomId === socket.id
+      );
+      if (index !== -1) {
+        players.splice(index, 1);
+        if (lobby.players.length === 0) {
+          const lobbyIndex = lobbies.findIndex((l) => l.id === lobby.id);
+          lobbies.splice(lobbyIndex, 1);
+        }
+      }
+    });
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
@@ -92,7 +128,9 @@ const getFirstPlayerToPlay = () => {
 };
 
 const getNextPlayer = (currentPlayerId) => {
-  const currentIndex = players.findIndex(player => player.roomId === currentPlayerId);
-  const nextIndex = (currentIndex + 1) % players.length; 
+  const currentIndex = players.findIndex(
+    (player) => player.roomId === currentPlayerId
+  );
+  const nextIndex = (currentIndex + 1) % players.length;
   return players[nextIndex].roomId;
 };
